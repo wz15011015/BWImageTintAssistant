4332c8c9f8a33fb11b595995f5cce33f56747ca7
wangzhi <wzqsyk@126.com>
2019-12-02 09:33:25 +0800

    应用程序关闭功能的优化。

--- ImageTintAssistant-Mac/ImageTintAssistant-Mac/AppDelegate.swift
+++ ImageTintAssistant-Mac/ImageTintAssistant-Mac/AppDelegate.swift
@@ -11,33 +11,15 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
-    private var shouldTeminateAppWhenClose: Bool = true
-
     // 应用程序名称默认为TARGET名称,可以在Building Setting --> Product Nmae 中修改.
-    
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         
-        // 1. 点击关闭按钮时,直接让应用程序退出
-        // 注册 willCloseNotification 通知,然后在通知方法中退出应用程序
+        // 注册通知
         NotificationCenter.default.addObserver(self, selector: #selector(applicationWindowWillClose), name: NSWindow.willCloseNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(applicationWindowWillEnterFullScreen), name: NSWindow.willEnterFullScreenNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(applicationWindowWillExitFullScreen), name: NSWindow.willExitFullScreenNotification, object: nil)
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-        print("applicationWillTerminate")
-    }
-    
-    func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
-        print("applicationShouldTerminate")
-        
-        // 返回 .terminateCancel 时,终止进程将会停止,并将控制权交回给主事件循环
-        // 返回 .terminateNow 时,该方法会向通知中心发布 willTerminateNotification 消息
-        // 返回 .terminateLater 时,则该应用程序将以modalPanel模式运行其RunLoop,直到调用toApplicationShouldTerminate:方法为止
-        return NSApplication.TerminateReply.terminateNow
     }
     
     func applicationWillBecomeActive(_ notification: Notification) {
@@ -54,29 +36,44 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // command + H
         print("applicationWillHide")
     }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+        print("applicationWillTerminate")
+    }
+    
+    func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
+        print("applicationShouldTerminate")
+        
+        // 返回 .terminateCancel 时,终止进程将会停止,并将控制权交回给主事件循环
+        // 返回 .terminateNow 时,该方法会向通知中心发布 willTerminateNotification 消息
+        // 返回 .terminateLater 时,则该应用程序将以modalPanel模式运行其RunLoop,直到调用toApplicationShouldTerminate:方法为止
+        return NSApplication.TerminateReply.terminateNow
+    }
+    
+    /// 当关闭最后一个窗口时,退出应用程序
+    /// - Parameter sender: 应用程序
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        // true: 窗口 和 应用程序 都关闭
+        // false: 窗口 关闭
+        return true
+    }
     
     
     // MARK: - Custom Notification
     
+    @objc func applicationWindowWillClose(_ aNotification: Notification) {
+        print("NSWindow.willCloseNotification")
+        
+//        NSApp.terminate(self)
+    }
+    
     @objc func applicationWindowWillEnterFullScreen(_ aNotification: Notification) {
         print("NSWindow.willEnterFullScreenNotification")
-        
-        shouldTeminateAppWhenClose = true
     }
     
     @objc func applicationWindowWillExitFullScreen(_ aNotification: Notification) {
         print("NSWindow.willExitFullScreenNotification")
-        
-        shouldTeminateAppWhenClose = false
-    }
-    
-    @objc func applicationWindowWillClose(_ aNotification: Notification) {
-        print("NSWindow.willCloseNotification")
-        
-        // 解决退出全屏时,也会发送willCloseNotification通知,从而导致退出应用程序的问题
-        guard shouldTeminateAppWhenClose else { return }
-        
-        NSApp.terminate(self)
     }
 }
 
